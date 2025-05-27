afc40154fc9c0fed2410352ef8b9f2189e427527
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-05-27 16:36:59 -0300

    limit and aliasing

--- commands/scripts-sql/limit-aliasing-tutorial.sql
+++ commands/scripts-sql/limit-aliasing-tutorial.sql
@@ -2,4 +2,13 @@
 
 SELECT *
 FROM employee_demographics
-LIMIT 3
+ORDER BY age DESC
+LIMIT 2, 1
+
+
+-- Aliasing 
+
+SELECT gender, AVG(age) AS avg_age 
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40
